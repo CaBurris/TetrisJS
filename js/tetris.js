2c58e4ab8ddb7b5b4b2630c293a8aa5ed9e79127
Christy <caburris@ucdavis.edu>
2018-04-04 16:54:28 -0700

    created first moving piece with dropCounter and dropInterval

--- js/tetris.js
+++ js/tetris.js
@@ -3,14 +3,20 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);  //increase object size
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
 
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0],
 ]; //add the middle array one so object will rotate around the center vs flipping up and down
+
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawMatrix(player.matrix, player.pos);
+}
+
 function drawMatrix(matrix, offset){
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
@@ -24,9 +30,28 @@ function drawMatrix(matrix, offset){
     });
 }
 
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0) {
+    const deltaTime = time- lastTime;
+    lastTime = time;
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
+    console.log(time);
+    draw();
+    requestAnimationFrame(update);
+}
+
 const player = {
     pos: {x: 5, y: 5},
     matrix: matrix,
 }
 
-drawMatrix(player.matrix, player.pos);
+update();
